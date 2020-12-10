d735b40b26b4cbfe4ddd9819a6ec7612316779d9
WalkerKarina <karina.walker.nicky@gmail.com>
2020-12-10 16:06:21 +0900

    git server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fabric } from 'fabric';
+import { fabric } from "fabric";
 import "./styles.css";
 
 var model;
@@ -8,24 +8,24 @@ var mousePressed = false;
 var classNames = [];
 var names;
 
-$(function() {
-  canvas = window._canvas = new fabric.Canvas('canvas');
-  canvas.backgroundColor = '#ffffff';
+$(function () {
+  canvas = window._canvas = new fabric.Canvas("canvas");
+  canvas.backgroundColor = "#ffffff";
   canvas.isDrawingMode = 0;
   canvas.freeDrawingBrush.color = "black";
   canvas.freeDrawingBrush.width = 10;
   canvas.renderAll();
 
   //Event listeners for mouse movement
-  canvas.on('mouse:up', function(e) {
-      getPrediction();
-      mousePressed = false
+  canvas.on("mouse:up", function (e) {
+    getPrediction();
+    mousePressed = false;
   });
-  canvas.on('mouse:down', function(e) {
-      mousePressed = true
+  canvas.on("mouse:down", function (e) {
+    mousePressed = true;
   });
-  canvas.on('mouse:move', function(e) {
-      recordCoor(e)
+  canvas.on("mouse:move", function (e) {
+    recordCoor(e);
   });
 });
 
@@ -35,191 +35,201 @@ function recordCoor(event) {
   var posY = pointer.y;
 
   if (posX >= 0 && posY >= 0 && mousePressed) {
-      coords.push(pointer)
+    coords.push(pointer);
   }
 }
 
 function getImageData() {
-  //get the minimum bounding box around the drawing 
-  const mbb = getMinBox()
+  //get the minimum bounding box around the drawing
+  const mbb = getMinBox();
 
-  //get image data according to device pixel ratio 
-  const dpi = window.devicePixelRatio
-  const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
-                                                (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+  //get image data according to device pixel ratio
+  const dpi = window.devicePixelRatio;
+  const imgData = canvas.contextContainer.getImageData(
+    mbb.min.x * dpi,
+    mbb.min.y * dpi,
+    (mbb.max.x - mbb.min.x) * dpi,
+    (mbb.max.y - mbb.min.y) * dpi
+  );
   imgData.crossOrigin = "Anonymous";
-  return imgData
+  return imgData;
 }
 
 function getMinBox() {
-  //get coordinates 
+  //get coordinates
   var coorX = coords.map((point) => {
-      return point.x
+    return point.x;
   });
   var coorY = coords.map((point) => {
-      return point.y
+    return point.y;
   });
 
-  //find top left and bottom right corners 
+  //find top left and bottom right corners
   var min_coords = {
-      x: Math.min.apply(null, coorX),
-      y: Math.min.apply(null, coorY)
-  }
+    x: Math.min.apply(null, coorX),
+    y: Math.min.apply(null, coorY),
+  };
   var max_coords = {
-      x: Math.max.apply(null, coorX),
-      y: Math.max.apply(null, coorY)
-  }
+    x: Math.max.apply(null, coorX),
+    y: Math.max.apply(null, coorY),
+  };
 
   return {
-      min: min_coords,
-      max: max_coords
-  }
+    min: min_coords,
+    max: max_coords,
+  };
 }
 
 function allowDrawing() {
   canvas.isDrawingMode = 1;
   // document.getElementById('status').innerHTML = 'Model Loaded';
-  $('button').prop('disabled', false);
+  $("button").prop("disabled", false);
   //var slider = document.getElementById('myRange');
   // slider.oninput = function() {
   //     canvas.freeDrawingBrush.width = this.value;
-  // };  
+  // };
 }
 
 function preprocess(imgData) {
   return tf.tidy(() => {
-    //convert to a tensor 
-    let tensor = tf.browser.fromPixels(imgData, 1)
-    
-    //resize 
-    const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
-    
-    //normalize 
+    //convert to a tensor
+    let tensor = tf.browser.fromPixels(imgData, 1);
+
+    //resize
+    const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat();
+
+    //normalize
     const offset = tf.scalar(255.0);
     const normalized = tf.scalar(1.0).sub(resized.div(offset));
 
-    //We add a dimension to get a batch shape 
-    const batched = normalized.expandDims(0)
-    return batched
-  })
+    //We add a dimension to get a batch shape
+    const batched = normalized.expandDims(0);
+    return batched;
+  });
 }
 
 function getClassNames(indices) {
-  var outp = []
-  for (var i = 0; i < indices.length; i++)
-    outp[i] = classNames[indices[i]]
-  return outp
+  var outp = [];
+  for (var i = 0; i < indices.length; i++) outp[i] = classNames[indices[i]];
+  return outp;
 }
 
 async function loadDict() {
-  //var url = 'http://127.0.0.1:8000/model/classNames.txt';
-  var url = './model/classNames.txt';
+  //var url = "http://127.0.0.1:8000/model/class_names.txt";
+  var url = "./class_names.txt";
 
   fetch(url)
-  .then(res => res.text())
-  .then((data) => {
-    const list = data.split(/\n/)
-    for (var i = 0; i < list.length - 1; i++) {
-        let symbol = list[i]
-        classNames[i] = symbol
-    }
-    randomClassNameFirst()
-  })
-  .catch(err => { throw err });
+    .then((res) => res.text())
+    .then((data) => {
+      const list = data.split(/\n/);
+      for (var i = 0; i < list.length - 1; i++) {
+        let symbol = list[i];
+        classNames[i] = symbol;
+      }
+      randomClassNameFirst();
+    })
+    .catch((err) => {
+      throw err;
+    });
 }
 
 //find index of x (thing drawn)
 function findIndicesOfMax(inp, count) {
-    var outp = [];
-    for (var i = 0; i < inp.length; i++) {
-        outp.push(i); // add index to output array
-        if (outp.length > count) {
-            outp.sort(function(a, b) {
-                return inp[b] - inp[a];
-            }); // descending sort the output array
-            outp.pop(); // remove the last index (index of smallest element in output array)
-        }
+  var outp = [];
+  for (var i = 0; i < inp.length; i++) {
+    outp.push(i); // add index to output array
+    if (outp.length > count) {
+      outp.sort(function (a, b) {
+        return inp[b] - inp[a];
+      }); // descending sort the output array
+      outp.pop(); // remove the last index (index of smallest element in output array)
     }
-    return outp;
+  }
+  return outp;
 }
 
 //number of times x is identified
 function findTopValues(inp, count) {
-    var outp = [];
-    let indices = findIndicesOfMax(inp, count)
-    // show 5 greatest scores
-    for (var i = 0; i < indices.length; i++)
-        outp[i] = inp[indices[i]]
-    return outp
+  var outp = [];
+  let indices = findIndicesOfMax(inp, count);
+  // show 5 greatest scores
+  for (var i = 0; i < indices.length; i++) outp[i] = inp[indices[i]];
+  return outp;
 }
 
 function getPrediction() {
-    //make sure we have at least two recorded coordinates 
+  //make sure we have at least two recorded coordinates
   if (coords.length >= 2) {
+    //get the image data from the canvas
+    const imgData = getImageData();
 
-    //get the image data from the canvas 
-    const imgData = getImageData()
+    //get the prediction
+    const pred = model.predict(preprocess(imgData)).dataSync();
 
-    //get the prediction 
-    const pred = model.predict(preprocess(imgData)).dataSync()
-
-    //find the top 5 predictions 
-    const indices = findIndicesOfMax(pred, 5)
-    const probs = findTopValues(pred, 5) 
-    names = getClassNames(indices)
-    console.log("Names: ")
-    console.log(names)
-    display()
+    //find the top 5 predictions
+    const indices = findIndicesOfMax(pred, 5);
+    const probs = findTopValues(pred, 5);
+    names = getClassNames(indices);
+    console.log("Names: ");
+    console.log(names);
+    display();
   }
 }
 var maxAppend = 0;
 
-function display(){
-  for (var i=0; i<names.length; i++ ){
+function display() {
+  for (var i = 0; i < names.length; i++) {
     if (maxAppend < 5) {
-      $( "#list" ).append('<li>' + names[i].replace("_", " ") + '</li>')
-      maxAppend = maxAppend + 1
+      $("#list").append("<li>" + names[i].replace("_", " ") + "</li>");
+      maxAppend = maxAppend + 1;
     } else if (maxAppend >= 5) {
-      $( "#list li" ).first().remove()
-      $( "#list" ).append('<li>' + names[i].replace("_", " ") + '</li>')
+      $("#list li").first().remove();
+      $("#list").append("<li>" + names[i].replace("_", " ") + "</li>");
     }
   }
 }
 
-async function loadModel() {  
-  //const path = 'http://127.0.0.1:8000/model/model.json';
-  const path = './model/model.json';
-  model = await tf.loadLayersModel(path)
+async function loadModel() {
+  //const path = "http://127.0.0.1:8000/model/model.json";
+  const path = "./model.json";
+  model = await tf.loadLayersModel(path);
 
-  console.log("Model: ")
-  console.log(model)
+  console.log("Model: ");
+  console.log(model);
 
-  console.log("Model Prediction trial: ")
-  console.log(model.predict(tf.zeros([1, 28, 28, 1])))
+  console.log("Model Prediction trial: ");
+  console.log(model.predict(tf.zeros([1, 28, 28, 1])));
 
-  allowDrawing()
+  allowDrawing();
 
-  await loadDict()
+  await loadDict();
 }
 
 loadModel();
 
-$( "#erase" ).click( function(){
+$("#erase").click(function () {
   canvas.clear();
-  canvas.backgroundColor = '#ffffff';
+  canvas.backgroundColor = "#ffffff";
   coords = [];
-  console.log("Running erase from js")
+  console.log("Running erase from js");
 });
 
 function randomClassNameFirst() {
-  var randomNum = Math.floor(Math.random()*10);
-  var command = classNames[randomNum].replace("_", " ")
-  $( "#command" ).append('<br></br> <span style="text-align: center; border:2px solid #89cff0; padding: 10px; color: #89cff0; margin: 10px;">' + command + '</span>')
+  var randomNum = Math.floor(Math.random() * 10);
+  var command = classNames[randomNum].replace("_", " ");
+  $("#command").append(
+    '<br></br> <span style="text-align: center; border:2px solid #89cff0; padding: 10px; color: #89cff0; margin: 10px;">' +
+      command +
+      "</span>"
+  );
 }
 
-$( "#randomClassNameAppend" ).click( function(){
-  var randomNum = Math.floor(Math.random()*10)
-  var command = classNames[randomNum].replace("_", " ")
-  $( "#command" ).html('<br></br> <span style="text-align: center; border:2px solid #89cff0; padding: 10px; color: #89cff0; margin: 10px;">' + command + '</span>')
-
+$("#randomClassNameAppend").click(function () {
+  var randomNum = Math.floor(Math.random() * 10);
+  var command = classNames[randomNum].replace("_", " ");
+  $("#command").html(
+    '<br></br> <span style="text-align: center; border:2px solid #89cff0; padding: 10px; color: #89cff0; margin: 10px;">' +
+      command +
+      "</span>"
+  );
 });
